68559ff8cf0e5d208a4b1df12119de8b7c89c278
jfinocchiaro <22jjf22@gmail.com>
2017-11-11 10:48:32 -0700

    Updated agents and have the beginnings of a linear optimization.

--- agents.py
+++ agents.py
@@ -3,19 +3,22 @@ import numpy as np
 
 
 class Agent:
-    def __init__(self, id, utility, endowment, endowment_plan, prices, subplans):
-        self.id = id
+    def __init__(self, idnum, utility, endowment,  prices, subplans, r, k):
+        self.idnum = idnum
         self.u = utility #function
-        self.e = endowment #vector dim c x 1
-        self.endowment_plan = endowment_plan #how much the person would have after they sell if they sold right now
-        self.p = prices #vector dim c x 1
-        self.subplans = subplans #vector dimension (num_agents) x 1
+        self.e = endowment.T #vector dim c x 1
+        self.endowment_plan = endowment.T #how much the person would have after they sell if they sold right now
+        self.p = prices.T #vector dim c x 1
+        self.subplans = subplans #vector dimension (num_agents) x c
         self.x = np.sum(subplans)
-
-        #self.budget_constraint = sum(np.multiply(p, e))
+        self.r = r.T #vector dim c x 1, commodity to resell
+        self.k = k.T #vector dim c x 1, commodity to keep
+        self.budget_constraint_ineq = np.matmul(self.p, (self.endowment_plan + r))
+        self.budget_constraint_eq = (r + k).T
 
     def budgetconstraint(self, G):
-        self.budget_constraint = 0
+        budget_constraint = 0
 
         for neighbor in G.neighbors(self.id):
-            self.budget_constraint += subplans[neighbor]
+            agent_neighbor = G.node[neighbor]['agentprop']
+            plan_fromneighbor = np.matmul(agent_neighbor.p.T, self.subplans)
--- convex_optimization_practice.py
+++ convex_optimization_practice.py
@@ -1,1 +1,56 @@
 from cvxopt import matrix, solvers
+import agents
+from agents import Agent
+from pulp import *
+import networkx as nx
+import numpy as np
+from collections import defaultdict
+
+def optimizeutility(agent, G, agentlist):
+    print agent.idnum
+
+    opt_prob = LpProblem("Optimize utility", LpMaximize)
+
+    xs = [LpVariable("x{}".format(i+1), cat="Continuous") for i in range(len(agent.e))]
+
+    # Objective function
+    opt_prob += agent.u, "Optimization Function- agent utility"
+
+
+
+    # add constraint
+    total_spending = 0
+
+    for nei in G.neighbors(agent.idnum):
+        neighbor = agentlist[nei]
+        print neighbor.e
+        total_spending += np.dot(neighbor.p.T, xs)
+
+    print total_spending
+    opt_prob += total_spending <= agent.budget_constraint_ineq
+    opt_prob += sum(xs) == agent.budget_constraint_eq
+
+    opt_prob.solve()
+
+    if opt_prob.status == LpStatusInfeasible:
+            print "Constraints are infeasible!"
+
+    if opt_prob.status == LpStatusUnbounded:
+            print "Solution is unbounded!"
+
+    if opt_prob.status == LpStatusOptimal:
+            print "Optimal solution exists and is equal to: {} and the optimal point is:".format(value(opt_prob.objective))
+            for variable in opt_prob.variables():
+                    print "{} = {}".format(variable.name, variable.varValue)
+
+G = nx.Graph()
+G.add_edge(1,2)
+c = 2
+agentlist = defaultdict(Agent)
+agent1 = Agent(1, 0, np.array((1,2)), np.array((1, 5)), np.array((0, 3)), np.array((0,0)), np.array((0,0)))
+agentlist[1] = agent1
+
+agent2 = Agent(1, 0, np.array((1,2)), np.array((1, 5)), np.array((0, 3)), np.array((0,0)), np.array((0,0)))
+agentlist[2] = agent2
+
+optimizeutility(agent1, G, agentlist)
