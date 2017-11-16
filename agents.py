b4c8f6419f7fff04f1e47c9dfdbf6fa34c3d5d2a
jfinocchiaro <22jjf22@gmail.com>
2017-11-16 13:56:40 -0700

    Work in lab.  Still need to figure out r and k$

--- agents.py
+++ agents.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Agent:
-    def __init__(self, idnum, utility, endowment,  prices, subplans, r, k):
+    def __init__(self, idnum, utility, endowment,  prices, subplans, lambdafunc):
         self.idnum = idnum
         self.u = utility.T #vector dim c x 1 of coefficients for each commodity c
         self.e = endowment.T #vector dim c x 1
@@ -12,10 +12,12 @@ class Agent:
         self.p = prices.T #vector dim c x 1
         self.subplans = subplans #vector dimension (num_agents) x c
         self.x = np.sum(subplans)
-        self.r = r.T #vector dim c x 1, commodity to resell
-        self.k = k.T #vector dim c x 1, commodity to keep
-        self.budget_constraint_ineq = np.matmul(self.p, (self.endowment_plan + r))
-        self.budget_constraint_eq = (r + k).T
+        self.lambdafunc = lambdafunc #cx1 vector of proportion of a good to resell
+        c = len(self.u)
+        self.r = np.zeros((c,1))
+        self.k = np.zeros((c,1))
+        self.budget_constraint_ineq = np.matmul(self.p, (self.e + self.r))
+        #self.budget_constraint_eq = (r + k).T
 
     def budgetconstraint(self, G):
         budget_constraint = 0
--- convex_optimization_practice.py
+++ convex_optimization_practice.py
@@ -1,4 +1,4 @@
-from cvxopt import matrix, solvers
+#from cvxopt import matrix, solvers
 import agents
 from agents import Agent
 from pulp import *
@@ -9,21 +9,32 @@ from collections import defaultdict
 def optimizelinearutility(agent, G, agentlist):
     num_goods = len(agent.e)
     opt_prob = LpProblem("Optimize utility", LpMaximize)
+    num_neighbors = len(G.neighbors(agent.idnum))
 
-    xs = [LpVariable("x{}".format(i+1), cat="Continuous") for i in range(num_goods)]
-    objective = np.dot(agent.u, np.array(xs))
+    xs = [];
+    for j in range(num_neighbors):
+        xs.append([LpVariable("x{}".format(i+1+j*num_goods), cat="Continuous") for i in range(num_goods)]);
+
+    print(np.array(xs))
+
+    objective = np.sum(np.dot(np.array(xs), agent.u)) #symbolic
+
+    print(objective)
     # Objective function
     opt_prob += objective, "Optimization Function- agent utility"
 
     # add constraint
+    neighbor_prices = []
     total_spending = 0
 
-    for nei in G.neighbors(agent.idnum):
+    for j, nei in enumerate(G.neighbors(agent.idnum)):
         neighbor = agentlist[nei]
-        print (neighbor.e)
-        total_spending += np.dot(neighbor.p.T, xs)
+        #print (neighbor.e)
+        #neighbor_prices.extend(neighbor.p.T)
+        total_spending += np.dot(neighbor.p.T, xs[j])
 
     print (total_spending )
+
     opt_prob += total_spending <= agent.budget_constraint_ineq
     #opt_prob += sum(xs) == agent.budget_constraint_eq
 
@@ -38,20 +49,35 @@ def optimizelinearutility(agent, G, agentlist):
     if opt_prob.status == LpStatusUnbounded:
             print ("Solution is unbounded!")
 
+
+    total_goods_purchased = np.zeros((num_goods, 1))
     if opt_prob.status == LpStatusOptimal:
             print ("Optimal solution exists and is equal to: {} and the optimal point is:".format(value(opt_prob.objective)) )
-            for variable in opt_prob.variables():
+            for count, variable in enumerate(opt_prob.variables()):
                     print ("{} = {}".format(variable.name, variable.varValue))
+                    total_goods_purchased[(count % num_goods)] += variable.varValue
+
+    print(total_goods_purchased)
+    agent.r = np.multiply(agent.lambdafunc,total_goods_purchased)
+
+
+
+
 
 G = nx.Graph()
 G.add_edge(1,2)
+G.add_edge(2,3)
 c = 2
 agentlist = defaultdict(Agent)
 #id num, utility, endowment, prices, subplans, r, k
-agent1 = Agent(1, np.array((2,3)), np.array((300,100)), np.array((1, 5)), np.array([[0,0], [3,0]]), np.array((0,0)), np.array((0,0)))
+agent1 = Agent(1, np.array((10,1)), np.array((0,1)), np.array((10, 10)), np.array([[0,0], [3,0]]), np.array([0,1]))
 agentlist[1] = agent1
 
-agent2 = Agent(2, np.array((5,2)), np.array((100,200)), np.array((2, 5)), np.array([[0, 3], [0,0]]), np.array((0,0)), np.array((0,0)))
+agent2 = Agent(2, np.array((10,10)), np.array((0,0)), np.array((10,10)), np.array([[0, 3], [0,0]]), np.array([1, 1]))
 agentlist[2] = agent2
 
-optimizelinearutility(agent1, G, agentlist)
+agent3 = Agent(3, np.array((1,10)), np.array((1,0)), np.array((10,10)), np.array([[0,0], [3,0]]), np.array([1, 0]))
+agentlist[3] = agent3
+
+
+optimizelinearutility(agent2, G, agentlist)
